f87d20a7108eae084c891aae17c29e2225c4ae87
thinkcomplexion-arch <thinkcomplexion@gmail.com>
2026-07-30 09:06:02 +0530

    Remove error reporting from ErrorComponent
    Removed error reporting in ErrorComponent.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -37,9 +37,7 @@ function NotFoundComponent() {
 function ErrorComponent({ error, reset }: { error: Error; reset: () => void }) {
   console.error(error);
   const router = useRouter();
-  useEffect(() => {
-    reportLovableError(error, { boundary: "tanstack_root_error_component" });
-  }, [error]);
+  
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-background px-4">
